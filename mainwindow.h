b88c9da03ac598b6d9c2419a1be041b36663a919
rodrigogomesrc <rocha.rodrigogomes@gmail.com>
2021-03-26 14:22:39 -0300

    usando o exemplo fornecido

--- mainwindow.h
+++ mainwindow.h
@@ -2,20 +2,35 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "trem.h"
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+
+public slots:
+    void updateInterface(int,int,int);
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
 
 private:
     Ui::MainWindow *ui;
+
+    //Cria os objetos TREM's
+    Trem *trem1;
+    Trem *trem2;
 };
+
 #endif // MAINWINDOW_H
